79091584437e00e0f0ad35237358db58bdd12202
sharvil1234 <71960080+sharvil1234@users.noreply.github.com>
2021-03-13 22:23:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+Webcam.set({
+    width: 310,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90,
+
+    constraints: {
+        facingMode: "environment"
+    }
+});
+
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
+    });
+}
+
+console.log('ml5 version: ', ml5.version);
+classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+
+function modelLoaded() {
+    console.log('Model is loaded');
+}
+
+function check() {
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(results);
+        document.getElementById("object_name").innerHTML += "Object -  " + results[0].label +"  ";
+        document.getElementById("object_name").innerHTML += "Accuracy -  " + results[0].confidence.toFixed(3);
+
+    }
+}
